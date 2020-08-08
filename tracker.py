f69cc187dda9445b1e3703551312073b0a5c53c1
Hoang Nguyen <mizore742@hotmail.com>
2020-08-08 18:01:47 -0400

    Changed chrome options

--- tracker.py
+++ tracker.py
@@ -13,7 +13,15 @@ phillyParkingUrl = "https://onlineserviceshub.com/ParkingPortal/Philadelphia"
 hondaCivicLicensePlate = os.environ.get("hoangLicensePlate")
 
 # Access the driver
-driver = webdriver.Chrome(executable_path=os.environ.get("chromeDriverPath"))
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("chromeDriverPath"), chrome_options=chrome_options)
+
+# Access the driver
+# driver = webdriver.Chrome(executable_path=os.environ.get("chromeDriverPath"))
 
 # Access the parking website
 driver.get(phillyParkingUrl)
